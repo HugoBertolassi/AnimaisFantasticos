c3cbf7b88a77de54ccc6758256b494ea0d516d3a
hugo <hugordura@hotmail.com>
2023-01-25 18:30:23 -0400

    Scroll- suave e com animacao das secoes

--- main.js
+++ main.js
@@ -54,5 +54,65 @@ function initAccordeon(){
     })
 }
 
+//aula 311
+
+function initScrollSuave(){
+    const linksInternos=document.querySelectorAll('.js-menu a[href^="#"]')//a[href^="#"] Valida se eh link interno
+    //console.log(linksInternos)
+
+    function scrollToSection(event){
+        event.preventDefault();//desabilita o comportamento padrao de funcionamento do link
+        //const href=event.currentTarget;//ele pega o elemento e queremos o valor
+        const href=event.currentTarget.getAttribute('href');//ele pega o elemento e queremos o valor
+        //console.log(href)
+
+        const section = document.querySelector(href);
+        //console.log(href,section)
+        
+        section.scrollIntoView({
+            behavior:"smooth",
+            block:"start",
+        });
+
+        //Forma alternativa de scrool suave
+        // const topo = section.offsetTop
+        // window.scrollTo({
+        //     top: topo,
+        //     behavior:"smooth"
+        // })
+    }
+
+    linksInternos.forEach((link)=>{
+        link.addEventListener('click',scrollToSection)
+    });
+}
+
+//aula12-funcao de animacao do scroll
+function initAnimacaoScroll(){
+    const sections=document.querySelectorAll(".js-scroll")
+    console.log(sections)
+    if(sections.length){
+        const windowMetade  =   window.innerHeight * 0.8;
+        
+        function animaScroll(){
+            sections.forEach((section)=>{
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible= (sectionTop - windowMetade)<0 ;
+
+                if(isSectionVisible){
+                    section.classList.add('ativo');
+                }
+                else{//desativa animacao
+                    section.classList.remove('ativo');
+                }
+            });
+        }
+    }
+    animaScroll();//Inicaliza para aparecer o primeiro conteudo sem depender do scroll da tela
+    window.addEventListener('scroll',animaScroll);
+}
+
 initTabNav();//aula 309
 initAccordeon();//aula 310
+initScrollSuave();//aula 311
+initAnimacaoScroll();//aula 312
